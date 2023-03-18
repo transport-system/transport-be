7d258c043d483184995ad28a7c54050088a35c2f
Nha Phan <phanthanhnha06@gmail.com>
2023-03-18 23:24:47 +0700

    Update and fix pom.xml

--- src/main/java/com/transport/transport/TransportApplication.java
+++ src/main/java/com/transport/transport/TransportApplication.java
@@ -2,15 +2,18 @@ package com.transport.transport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableScheduling
 public class TransportApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransportApplication.class, args);
         TimeZone.setDefault(TimeZone.getTimeZone("GMT+00:00"));;
+
     }
 }
--- src/main/java/com/transport/transport/config/paypal/PaypalConfig.java
+++ src/main/java/com/transport/transport/config/paypal/PaypalConfig.java
@@ -1,40 +1,43 @@
 package com.transport.transport.config.paypal;
 
-import com.paypal.base.rest.APIContext;
-import com.paypal.base.rest.OAuthTokenCredential;
-import com.paypal.base.rest.PayPalRESTException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.paypal.api.payments.PaymentHistory;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
 
 @Configuration
 public class PaypalConfig {
 
     @Value("${paypal.client.id}")
     private String clientId;
-    @Value("{paypal.client.secret}")
+    @Value("${paypal.client.secret}")
     private String clientSecret;
-    @Value("{paypal.mode}")
+    @Value("${paypal.mode}")
     private String mode;
 
     @Bean
-    public Map<String, String> paypalSdkConfig(){
+    public Map<String, String> paypalSdkConfig() {
         Map<String, String> configMap = new HashMap<>();
         configMap.put("mode", mode);
         return configMap;
     }
+
     @Bean
-    public OAuthTokenCredential oAuthTokenCrendential(){
-        return  new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+    public OAuthTokenCredential oAuthTokenCredential() {
+        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
     }
 
     @Bean
     public APIContext apiContext() throws PayPalRESTException {
-        APIContext context = new APIContext(oAuthTokenCrendential().getAccessToken());
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
         context.setConfigurationMap(paypalSdkConfig());
-        return apiContext();
+        return context;
     }
 }
